28de3dfd847ab716efd0c3f25b87d9c7392df404
neha patel <neha.patel@infiraise.com>
2023-12-01 14:13:47 +0530

    user change

--- app/Http/Controllers/API/AddUserController.php
+++ app/Http/Controllers/API/AddUserController.php
@@ -14,24 +14,14 @@ class AddUserController extends Controller
         public function add_user(Request $request)
         {
             $token = $request->bearerToken();
-            //$token= $token_data->token;
             $secretKey ='345fgvvc4';
             $decryptedInput = decrypt($token, $secretKey);
-            $token_data=list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
-            
-            $company_id=$token_data['0'];
-            $latest_employee_id = Company_Admins::latest('_id')->value('_id');
-            // dd($latest_employee_id);
-            
-
-            if($company_id==$latest_employee_id)
-            {
-                $latest_total_employee = Company_Admins::latest('_id')->value('total_employee');
-                // dd($latest_total_employee);
-            }
-            else{
-                $latest_total_employee = 0;
-            }
+            list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
+            $company_id=$id;
+            $comapny_admins=Company_Admins::where('_id',2)->value('total_employee');
+            // dd($comapny_admins);
+            $comapny_users=Company_user::where('company_id',$company_id)->count();
+           
             $validatedData = $request->validate([
                     'user_email' => 'required',
                     'user_name'=>'required',
@@ -43,48 +33,53 @@ class AddUserController extends Controller
             // $password = Hash::make($validatedData['user_password']);
             $password = hash('sha1',$request->password);
             $new_id = Company_user::max('_id') + 1;
-            $data = [
-                '_id' => $new_id,
-                'company_id'=>$company_id,
-                'counter'=>$latest_total_employee,
-                'user_email' => $validatedData['user_email'],
-                'user_name' => $validatedData['user_name'],
-                'user_password' =>$password,
-                'user_type' => $validatedData['user_type'],
-                'user_add_date' => $validatedData['user_add_date'],
-                'otp' => 0,
-                'otpexperience' => '',
-                'last_change_password' => '',
-                'last_login' => '',
-                'entry_time' => '',
-                'user_status' => '',
-                'shift_id' => '',
-                'employee' => '',
-                'payroll' => '',
-                'attendance' => '',
-                'break' => '',
-                'leave' => '',
-                'letter' => '',
-                'administration' => '',
-                'recruitment' => '',
-                'ip' => '',
-                'browser' => '',
-                'city' => '',
-                'state' => '',
-                'os' => '',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
 
-       $result = Company_user::insert($data);
 
+            if($comapny_admins >=  $comapny_users){
+                $data = [
+                    '_id' => $new_id,
+                    'company_id'=>$company_id,
+                    'counter'=>0,
+                    'user_email' => $validatedData['user_email'],
+                    'user_name' => $validatedData['user_name'],
+                    'user_password' =>$password,
+                    'user_type' => $validatedData['user_type'],
+                    'user_add_date' => $validatedData['user_add_date'],
+                    'otp' => 0,
+                    'otpexperience' => '',
+                    'last_change_password' => '',
+                    'last_login' => '',
+                    'entry_time' => '',
+                    'user_status' => '',
+                    'shift_id' => '',
+                    'employee' => '',
+                    'payroll' => '',
+                    'attendance' => '',
+                    'break' => '',
+                    'leave' => '',
+                    'letter' => '',
+                    'administration' => '',
+                    'recruitment' => '',
+                    'ip' => '',
+                    'browser' => '',
+                    'city' => '',
+                    'state' => '',
+                    'os' => '',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+               $result = Company_user::insert($data);
+            }
+            else{
+                dd("Plaese Check");
+            }
     
-    if ($result) {
-        return response()->json(['message' => 'User Adder successfully'], 201);
-    } else {
-        return response()->json(['message' => 'Failed to Add user'], 500);
+        if ($result) {
+            return response()->json(['message' => 'User Adder successfully'], 201);
+        } else {
+            return response()->json(['message' => 'Failed to Add user'], 500);
+        }
     }
-}
 
 
     }
--- app/Http/Controllers/API/CompanyAdminsController.php
+++ app/Http/Controllers/API/CompanyAdminsController.php
@@ -178,42 +178,48 @@ class CompanyAdminsController extends Controller
 
                 if ($user) {
                 foreach($user as $u){
+                    
                         if($u != ''){
                         if($u->emailVerificationStatus == '1'){
-                        $userModel = new Company_Admins(); // Create a new instance of the User model
-                        $userModel->companyID = $u->_id;
-                        $userModel->userEmail = $u->company_email;
-                        $userModel->userPass = $u->password;
+                        $user_data = new Company_Admins(); // Create a new instance of the user_data model
+                        $user_data->companyID = $u->_id;
+                        $user_data->userEmail = $u->company_email;
+                        $user_data->userPass = $u->password;
                         $token_data = TokenHandler::where(['company_id'=>$u->_id])->first();
                         $token= $token_data->token;
                         $secretKey ='345fgvvc4';
                         $decryptedInput = decrypt($token, $secretKey);
                         $token_data->token=list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
-                        $userModel->token = $token;
+                        $user_data->token = $token;
                         }
                         else{
-                            $userModel = "Email Not Verified";
+                            $user_data = "Email Not Verified";
                         }
                     }
+                   
                   }
                 }
-                if ($company_users) {  
+               
+                // dd($user_data);
+                if($company_users) {  
                     foreach($company_users as $u){
                         if($u != ''){
-                        $userModel = new Company_user(); // Create a new instance of the User model
-                        $userModel->id = $u->_id;
-                        $userModel->companyID = $u->company_id;
-                        $userModel->userEmail = $u->user_email;
+                        $company_user_data = new Company_user(); // Create a new instance of the User model
+                        $company_user_data->id = $u->_id;
+                        $company_user_data->companyID = $u->company_id;
+                        $company_user_data->userEmail = $u->user_email;
                         $token_data = TokenHandler::where(['company_id'=>$u->_id])->first();
                         $token= $token_data->token;
                         $secretKey ='345fgvvc4';
                         $decryptedInput = decrypt($token, $secretKey);
                         $token_data->token=list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
-                        $userModel->token = $token;
+                        $company_user_data->token = $token;
                         }
                     }
-                }
-            }   
+                    }
+                   
+                }   
+                
             if($type == 'admin')
             {
                
@@ -236,8 +242,20 @@ class CompanyAdminsController extends Controller
                         }
                     }
                 }
+               
             }
-            if($userModel != ''){
+            
+            if($user_data != ''){
+                $result=$user_data;
+                $success = true;
+                $message = "Login Succesfully";
+            }
+            elseif($company_users != ''){
+                $result=$company_user_data;
+                $success = true;
+                $message = "Login Succesfully";
+            }
+            elseif($admin != ''){
                 $result=$userModel;
                 $success = true;
                 $message = "Login Succesfully";
