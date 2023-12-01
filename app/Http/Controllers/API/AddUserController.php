b5bcab434188e8131b0b48dec7fd3bdef37172e5
chetnamalode <priti.m@veravalonline.com>
2023-12-01 10:02:00 +0530

    add_user according to total_employee

--- app/Http/Controllers/API/AddUserController.php
+++ app/Http/Controllers/API/AddUserController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Hash;
 
 class AddUserController extends Controller
 {
-        public function add_user(Request $request)
+        public function add_user(Request $request) //done
         {
             $token = $request->bearerToken();
             //$token= $token_data->token;
@@ -21,99 +21,138 @@ class AddUserController extends Controller
             
             $company_id=$token_data['0'];
             $latest_employee_id = Company_Admins::latest('_id')->value('_id');
-            // dd($latest_employee_id);
-            
-
-
-            $company_id=intval($id);
-        //    dd($company_id);
-            $company_admins=Company_Admins::where('_id',$company_id)->value('total_employee');
-            //  dd($company_admins);//changes
-
-            if($company_id==$latest_employee_id)
-            {
-                $latest_total_employee = Company_Admins::latest('_id')->value('total_employee');
-                // dd($latest_total_employee);
-            }
-            else{
-                $latest_total_employee = 0;
-            }
-            $validatedData = $request->validate([
-                    'user_email' => 'required',
-                    'user_name'=>'required',
-                    'user_password'=>'required',
-                    'user_type'=>'required',
-                    'user_add_date'=>'required',
-                
-            ]);
-            // $password = Hash::make($validatedData['user_password']);
-            $password = hash('sha1',$request->password);
-            $new_id = Company_user::max('_id') + 1;
-            $data = [
-                '_id' => $new_id,
-                'company_id'=>$company_id,
-                'counter'=>$latest_total_employee,
-                'user_email' => $validatedData['user_email'],
-                'user_name' => $validatedData['user_name'],
-                'user_password' =>$password,
-                'user_type' => $validatedData['user_type'],
-                'user_add_date' => $validatedData['user_add_date'],
-                'otp' => 0,
-                'otpexperience' => '',
-                'last_change_password' => '',
-                'last_login' => '',
-                'entry_time' => '',
-                'user_status' => '',
-                'shift_id' => '',
-                'employee' => '',
-                'payroll' => '',
-                'attendance' => '',
-                'break' => '',
-                'leave' => '',
-                'letter' => '',
-                'administration' => '',
-                'recruitment' => '',
-                'ip' => '',
-                'browser' => '',
-                'city' => '',
-                'state' => '',
-                'os' => '',
-                'delete_status'=>1,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ];
-
-              $result = Company_user::insert($data);
-
+            $company_id=intval($id);//fetch company_id
+            $company_admins=Company_Admins::where('_id',$company_id)->value('total_employee'); //fetch total employee from company_admin
+            $total=Company_user::where('company_id',$company_id)->count();//user count that particular id
+            $company_admins = Company_Admins::where('_id', $company_id)->first();  //get latest record from company_admin
+           if ($company_admins) {
+                $allowed_total_employee = $company_admins->total_employee;
+        
+              $latest_total_employee = Company_Admins::latest('_id')->value('total_employee');
+                // Check if the current number of employees is less than the allowed total employees
+              
+                if ($total < $allowed_total_employee) {
+                    // Continue with user creation
+                    $validatedData = $request->validate([
+                        'user_email' => 'required',
+                        'user_name'=>'required',
+                        'user_password'=>'required',
+                        'user_type'=>'required',
+                        'user_add_date'=>'required',
+                    
+                ]);
+                // $password = Hash::make($validatedData['user_password']);
+                $password = hash('sha1',$request->password);
+                $new_id = Company_user::max('_id') + 1;
+                $data = [
+                    '_id' => $new_id,
+                    'company_id'=>$company_id,
+                    'counter'=>$latest_total_employee,
+                    'user_email' => $validatedData['user_email'],
+                    'user_name' => $validatedData['user_name'],
+                    'user_password' =>$password,
+                    'user_type' => $validatedData['user_type'],
+                    'user_add_date' => $validatedData['user_add_date'],
+                    'otp' => 0,
+                    'otpexperience' => '',
+                    'last_change_password' => '',
+                    'last_login' => '',
+                    'entry_time' => '',
+                    'user_status' => '',
+                    'shift_id' => '',
+                    'employee' => '',
+                    'payroll' => '',
+                    'attendance' => '',
+                    'break' => '',
+                    'leave' => '',
+                    'letter' => '',
+                    'administration' => '',
+                    'recruitment' => '',
+                    'ip' => '',
+                    'browser' => '',
+                    'city' => '',
+                    'state' => '',
+                    'os' => '',
+                    'delete_status'=>1,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
     
-            if ($result) {
-        return response()->json(['message' => 'User Adder successfully'], 201);
-       } else {
-        return response()->json(['message' => 'Failed to Add user'], 500);
-       }
-    }
-public function update_user(Request $request ,$id)
+        
+                $result = Company_user::insert($data);
+        
+                    if ($result) {
+                        return response()->json(['message' => 'User added successfully'], 201);
+                    } else {
+                        return response()->json(['message' => 'Failed to add user'], 500);
+                    }
+                } else {
+                    return response()->json(['message' => 'Maximum number of employees reached for this company'], 400);
+                }
+            } else {
+                return response()->json(['message' => 'Company not found'], 404);
+            }
+ }
+public function update_user(Request $request ,$id) //done
 {
     // dd($request);
     $new_id=intval($id);
     $data = Company_user::where('_id',$new_id)->first();
-    $data->user_email = $request->user_email;
-    $data->user_name = $request->user_name;
-    $data->user_password= $request->user_password;
-    $data->user_type = $request->user_type;
-    $data->user_add_date = $request->user_add_date;
-    // dd($data);
-    $data->save();
-    
-    $response = [
-        "success" => true,
-        "data" => $data,
-        "message" => " data update Successfully !"
+    // dd($request);
+    $new_id=intval($id);
+    $data = Company_user::where('_id',$new_id)->first();
+    $validatedData = $request->validate([
+        'user_email' => 'required',
+        'user_name' => 'required',
+        'user_password' => 'required',
+        'user_type' => 'required',
+        'user_add_date' => 'required',
+    ]);
+
+    $password = hash('sha1', $request->user_password);
+    $data = [
+        'user_email' => $request['user_email'],
+        'user_name' => $request['user_name'],
+        'user_password' => $password,
+        'user_type' => $request['user_type'],
+        'user_add_date' => $request['user_add_date'],
+        'otp' => 0,
+        'otpexperience' => '',
+        'last_change_password' => '',
+        'last_login' => '',
+        'entry_time' => '',
+        'user_status' => '',
+        'shift_id' => '',
+        'employee' => '',
+        'payroll' => '',
+        'attendance' => '',
+        'break' => '',
+        'leave' => '',
+        'letter' => '',
+        'administration' => '',
+        'recruitment' => '',
+        'ip' => '',
+        'browser' => '',
+        'city' => '',
+        'state' => '',
+        'os' => '',
+        'delete_status'=>1,
+        'created_at' => now(),
+        'updated_at' => now(),
+        // Add other fields to be updated
     ];
-        return response()->json($response, 201);
+    // dd($data);
+   
+    $result = Company_user::where('_id', $new_id)->update($data);
+    // dd($result);
+        if ($result) {
+        return response()->json(['message' => 'User updated successfully'], 200);
+    } else {
+        return response()->json(['message' => 'Failed to update user'], 500);
+    }
    
 }
-public function delete_user($id)
+public function delete_user($id) //done
 {
    
     $new_id=intval($id);
@@ -129,7 +168,9 @@ public function index_user(Request $request)
     // dd($request);
         //   $token = $request->bearerToken();
         //   dd($token);
-        $records = User::all();
+        $records = Company_user::where('delete_status', 1)->get();//problem
+
+        // $records = Company_Admins::all();
         //dd($records);
         return response()->json(['success' => true, 'data' => $records], 200);
 }
