b4035796fc367d82cdc595a37fd03906881cb54f
chetnamalode <priti.m@veravalonline.com>
2023-12-04 09:40:14 +0530

    change

--- app/Http/Controllers/API/CompanyDepartmentController.php
+++ app/Http/Controllers/API/CompanyDepartmentController.php
@@ -18,18 +18,11 @@ class CompanyDepartmentController extends Controller
         $token_data = list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
         $company_id = $token_data['0'];
 
-        $latest_employee_id = Company_Admins::latest('_id')->value('_id');
-        $company_id = intval($id); //fetch company_id
-        $company_admins = Company_Admins::where('_id', $company_id)->value('total_employee'); //fetch total employee from company_admin
-        $total = Company_Department::where('company_id', $company_id)->count(); //user count that particular id
-        $company_admins = Company_Admins::where('_id', $company_id)->first();  //get latest record from company_admin
-        if ($company_admins) {
-            $allowed_total_employee = $company_admins->total_employee;
 
             $latest_total_employee = Company_Admins::latest('_id')->value('total_employee');
             // Check if the current number of employees is less than the allowed total employees
 
-            if ($total < $allowed_total_employee) {
+          
 
 
                 $validatedData = $request->validate([
@@ -55,12 +48,7 @@ class CompanyDepartmentController extends Controller
                 } else {
                     return response()->json(['message' => 'Failed to Add Department'], 500);
                 }
-            } else {
-                return response()->json(['message' => 'Maximum number of employees reached for this company'], 400);
-            }
-        } else {
-            return response()->json(['message' => 'Company not found'], 404);
-        }
+            
     }
 
 
--- app/Http/Controllers/API/CompanyHolidayController.php
+++ app/Http/Controllers/API/CompanyHolidayController.php
@@ -18,18 +18,9 @@ class CompanyHolidayController extends Controller
         $token_data = list($id, $user, $admin_name, $companyname) = explode('|', $decryptedInput);
         $company_id = $token_data['0'];
 
-        $latest_employee_id = Company_Admins::latest('_id')->value('_id');
-        $company_id = intval($id); //fetch company_id
-        $company_admins = Company_Admins::where('_id', $company_id)->value('total_employee'); //fetch total employee from company_admin
-        $total =Company_Holiday::where('company_id', $company_id)->count(); //user count that particular id
-        $company_admins = Company_Admins::where('_id', $company_id)->first();  //get latest record from company_admin
-        if ($company_admins) {
-            $allowed_total_employee = $company_admins->total_employee;
 
             $latest_total_employee = Company_Admins::latest('_id')->value('total_employee');
             // Check if the current number of employees is less than the allowed total employees
-
-            if ($total < $allowed_total_employee) {
 
                 $validatedData = $request->validate([
                     'holiday_name' => 'required',
@@ -59,12 +50,7 @@ class CompanyHolidayController extends Controller
                 } else {
                     return response()->json(['message' => 'Failed to Add Holiday'], 500);
                 }
-            } else {
-                return response()->json(['message' => 'Maximum number of employees reached for this company'], 400);
-            }
-        } else {
-            return response()->json(['message' => 'Company not found'], 404);
-        }
+             
     }
 
     public function update_holiday(Request $request) //done
